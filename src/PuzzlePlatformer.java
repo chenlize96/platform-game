b17317e7785da609df373fc17fcd833901fe4181
Lize Chen <chenlize96@gmail.com>
2019-11-25 15:37:13 -0700

    commit

--- src/PuzzlePlatformer.java
+++ src/PuzzlePlatformer.java
@@ -3,5 +3,6 @@ import javafx.application.Application;
 public class PuzzlePlatformer {
 	public static void main(String[] args) {
 		Application.launch(PuzzlePlatformerView.class, args);
+		//
 	}
 }
