721778a306d41c75f64a751ad36845e76cccc0a9
Suyang Chen <51867630+SuyangChen@users.noreply.github.com>
2019-11-25 17:26:21 -0700

    Add files via upload

---
+++ src/View/PuzzlePlatformer.java
@@ -0,0 +1,8 @@
+import javafx.application.Application;
+
+public class PuzzlePlatformer {
+	public static void main(String[] args) {
+		//Application.launch(PuzzlePlatformerView.class, args);
+		//
+	}
+}
